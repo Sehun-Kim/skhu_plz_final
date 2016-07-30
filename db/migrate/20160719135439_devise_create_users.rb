a57df09d0dd1307962e5146b005ae02c02b10a78
sehun_kim <iks987@naver.com>
2016-07-30 07:27:22 +0000

    이메일인증하고 다함 혹시 이메일 실행안되면 나한테 연락하셈

--- db/migrate/20160719135439_devise_create_users.rb
+++ db/migrate/20160719135439_devise_create_users.rb
@@ -4,11 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-<<<<<<< HEAD
 
-=======
-      
->>>>>>> 9a343294f3e61025159ddc29fadd85606f50ae47
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -24,18 +20,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       ## Confirmable
-<<<<<<< HEAD
        t.string   :confirmation_token
        t.datetime :confirmed_at
        t.datetime :confirmation_sent_at
        t.string   :unconfirmed_email # Only if using reconfirmable
-=======
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
->>>>>>> 9a343294f3e61025159ddc29fadd85606f50ae47
-
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
@@ -47,11 +35,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-<<<<<<< HEAD
-     add_index :users, :confirmation_token,   unique: true
-=======
-    # add_index :users, :confirmation_token,   unique: true
->>>>>>> 9a343294f3e61025159ddc29fadd85606f50ae47
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
