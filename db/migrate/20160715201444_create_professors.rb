4589941ef5dd06c1b8968e849b1fd0d9ae2214f8
resolver0106 <resolver0106@naver.com>
2016-08-01 07:39:11 +0000

    교수 버튼, 푸터 하단 고정 수정

--- db/migrate/20160715201444_create_professors.rb
+++ db/migrate/20160715201444_create_professors.rb
@@ -2,6 +2,7 @@ class CreateProfessors < ActiveRecord::Migration
   def change
     create_table :professors do |t|
       t.string :name
+      t.integer :dis
       t.timestamps null: false
     end
   end
