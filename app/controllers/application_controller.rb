288cab63bf1806eb98e28b2f2bebc714380e93ae
sehun <iks987@naver.com>
2016-08-17 06:43:59 +0000

    닉네임 뜨게 수정

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
@@ -6,5 +7,11 @@ class ApplicationController < ActionController::Base
   def authority_forbidden(error)
     Authority.logger.warn(error.message)
     redirect_to request.referrer.presence || root_path, :alert => '그럴 권한이 없습니다.'
+  end
+  
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
