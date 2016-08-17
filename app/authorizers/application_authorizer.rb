0ea291da06c0f5c475443c2e381975313d685c6d
resolver0106 <resolver0106@naver.com>
2016-08-17 09:05:52 +0000

    수정중111

--- app/authorizers/application_authorizer.rb
+++ app/authorizers/application_authorizer.rb
@@ -5,19 +5,12 @@ class ApplicationAuthorizer < Authority::Authorizer
   end
 
   def updatable_by?(user)
-<<<<<<< HEAD
     resource.user_id == user.id
   end
 
   def deletable_by?(user)
     resource.user_id == user.id
-=======
-    resource.user == user
   end
 
-  def deletable_by?(user)
-    resource.user == user
->>>>>>> 757ca0fdd669651c076604c41792f5a59d77e9ea
-  end
 
 end
